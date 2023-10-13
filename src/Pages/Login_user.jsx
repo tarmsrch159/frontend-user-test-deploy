b3adfa7c3f54385874c34898601496521bdecb6e
tarmsrch159 <tarmsrch159@gmail.com>
2023-10-13 20:20:02 +0700

    Updated user

--- src/Pages/Login_user.jsx
+++ src/Pages/Login_user.jsx
@@ -642,7 +642,7 @@ function Login_user() {
                                                     {/* recaptcha */}
                                                     <div class="mb-5 mt-5" style={{ display: 'flex', justifyContent: "center" }}>
                                                         < ReCAPTCHA
-                                                            sitekey={SITE_KEY}
+                                                            sitekey={SITE_KEY_2}
                                                             onChange={onChange}
                                                         />
                                                     </div>
@@ -836,57 +836,57 @@ function Login_user() {
                                                                                         </div>
                                                                                         :
                                                                                         <>
-                                                                                            <button
-                                                                                                type="button"
-                                                                                                className="btn btn-info"
-                                                                                                data-bs-toggle="modal"
-                                                                                                data-bs-target="#exampleModal"
-                                                                                                onClick={() => Export_certificate(val.reg_id)}
-                                                                                            >
-                                                                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
-                                                                                                    <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
-                                                                                                    <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z" />
-                                                                                                </svg>
-                                                                                            </button>
-                                                                                            {/* Modal */}
-                                                                                            <div
-                                                                                                className="modal fade"
-                                                                                                id="exampleModal"
-                                                                                                tabIndex={-1}
-                                                                                                aria-labelledby="exampleModalLabel"
-                                                                                                aria-hidden="true"
-                                                                                            >
-                                                                                                <div className="modal-dialog">
-                                                                                                    <div className="modal-content">
-                                                                                                        <div className="modal-header">
-                                                                                                            <h1 className="modal-title fs-5" id="exampleModalLabel">
+                                                                                           <button
+                                                                                            type="button"
+                                                                                            className="btn btn-primary"
+                                                                                            data-bs-toggle="modal"
+                                                                                            data-bs-target="#exampleModal"
+                                                                                            onClick={() => Export_certificate(val.reg_id)}
+                                                                                        >
+                                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                                                                                                <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+                                                                                                <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+                                                                                            </svg>
+                                                                                        </button>
+                                                                                        {/* Modal */}
+                                                                                        <div
+                                                                                            className="modal fade"
+                                                                                            id="exampleModal"
+                                                                                            tabIndex={-1}
+                                                                                            aria-labelledby="exampleModalLabel"
+                                                                                            aria-hidden="true"
+                                                                                        >
+                                                                                            <div className="modal-dialog">
+                                                                                                <div className="modal-content">
+                                                                                                    <div className="modal-header">
+                                                                                                        <h1 className="modal-title fs-5" id="exampleModalLabel">
 
-                                                                                                            </h1>
-                                                                                                            <button
-                                                                                                                type="button"
-                                                                                                                className="btn-close"
-                                                                                                                data-bs-dismiss="modal"
-                                                                                                                aria-label="Close"
-                                                                                                            />
-                                                                                                        </div>
-                                                                                                        <div className="modal-body">
-                                                                                                            <h5>ต้องการออกรายงานเกียรติบัตรใช่หรือไม่</h5>
-                                                                                                        </div>
-                                                                                                        <div className="modal-footer">
-                                                                                                            <button
-                                                                                                                type="button"
-                                                                                                                className="btn btn-secondary"
-                                                                                                                data-bs-dismiss="modal"
-                                                                                                            >
-                                                                                                                ยกเลิก
-                                                                                                            </button>
-                                                                                                            <button type="button" onClick={get_single_reg} className="btn btn-primary" data-bs-dismiss="modal">
-                                                                                                                ยืนยัน
-                                                                                                            </button>
-                                                                                                        </div>
+                                                                                                        </h1>
+                                                                                                        <button
+                                                                                                            type="button"
+                                                                                                            className="btn-close"
+                                                                                                            data-bs-dismiss="modal"
+                                                                                                            aria-label="Close"
+                                                                                                        />
+                                                                                                    </div>
+                                                                                                    <div className="modal-body">
+                                                                                                        <h5>ต้องการออกรายงานเกียรติบัตรใช่หรือไม่</h5>
+                                                                                                    </div>
+                                                                                                    <div className="modal-footer">
+                                                                                                        <button
+                                                                                                            type="button"
+                                                                                                            className="btn btn-secondary"
+                                                                                                            data-bs-dismiss="modal"
+                                                                                                        >
+                                                                                                            ยกเลิก
+                                                                                                        </button>
+                                                                                                        <button type="button" onClick={get_single_reg} className="btn btn-primary" data-bs-dismiss="modal">
+                                                                                                            ยืนยัน
+                                                                                                        </button>
                                                                                                     </div>
                                                                                                 </div>
                                                                                             </div>
+                                                                                        </div>
                                                                                         </>
                                                                                     }
                                                                                 </span>
